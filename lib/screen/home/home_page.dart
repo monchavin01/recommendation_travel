31b4aabf6bfb8367fd925dbb29a9572a028f6e25
monchavin <monchavin.pi@odds.team>
2020-12-15 22:52:55 +0700

    create home page

--- lib/screen/home/home_page.dart
+++ lib/screen/home/home_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:recomander_travel/constants/colors.dart';
+import 'package:get/get.dart';
+import 'package:flutter/cupertino.dart';
+
+import '../../rotes.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -8,8 +13,62 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            alignment: Alignment.center,
+            child: GestureDetector(
+              onTap: () {
+                try {
+                  Get.offAllNamed(Routes.recomend);
+                } catch (e) {
+                  print(e);
+                }
+                
+              },
+              child: Card(
+                clipBehavior: Clip.antiAliasWithSaveLayer,
+                shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(6),
+                ),
+                color: AppColors.errorColor,
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      width: Get.width / 1.05,
+                      height: Get.width / 1.5,
+                      color: AppColors.errorColor,
+                    )
+                  ],
+                ),
+              ),
+            )
+          ),
+          SizedBox(height: 10),
+          Container(
+            alignment: Alignment.center,
+            child: GestureDetector(
+              child: Card(
+                clipBehavior: Clip.antiAliasWithSaveLayer,
+                shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(6),
+                ),
+                color: AppColors.errorColor,
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      width: Get.width / 1.05,
+                      height: Get.width / 1.5,
+                      color: AppColors.errorColor,
+                    )
+                  ],
+                ),
+              ),
+            )
+          )
+        ],
+      ),
     );
   }
 }
