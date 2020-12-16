35bc1824accfcf600d38623775f9c8718c5eaa21
monchavin <monchavin.pi@odds.team>
2020-12-16 22:59:34 +0700

    add routes use get material

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:recomander_travel/rotes.dart';
 
 import 'main_screen.dart';
 
@@ -12,14 +14,15 @@ class _MyAppState extends State<MyApp> {
   @override
 
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    return GetMaterialApp(
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MainScreen(),
+      getPages: Routes.pages,
       debugShowCheckedModeBanner: false,
     );
   }
+  
 }
--- lib/rotes.dart
+++ lib/rotes.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:recomander_travel/main_screen.dart';
+import 'package:recomander_travel/screen/create_trip/create_trip.dart';
 import 'package:recomander_travel/screen/recomend/recomend_page.dart';
 
 class Routes {
+  static const rooted = '/';
   static const recomend = '/recomend';
+  static const createTrip = '/createTrip';
 
   static final pages = [
-    _page(recomend, RecomendPage())
+    _page(rooted, MainScreen()),
+    _page(recomend, RecomendPage()),
+    _page(createTrip, CreateTrip()),
   ];
 
   static GetPage _page(String path, Widget item) => GetPage(
