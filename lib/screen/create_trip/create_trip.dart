6c8dc5c75099ceb44296a6db11472f89db275d6e
monchavin <monchavin.pi@odds.team>
2020-12-16 23:04:43 +0700

    create page create trip

---
+++ lib/screen/create_trip/create_trip.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CreateTrip extends StatefulWidget {
+  @override
+  _CreateTripState createState() => _CreateTripState();
+}
+
+class _CreateTripState extends State<CreateTrip> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+    );
+  }
+}
